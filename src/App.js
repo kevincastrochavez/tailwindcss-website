b6583af2f3804808b6ff6794311e261f2cf56ed4
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-04-29 10:43:05 -0600

    finished setup of tailwindcss, created the navbar component and styled it, and imported the menu icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="app">Tailwind</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import App from "./App";
+
+import "../src/styles/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
